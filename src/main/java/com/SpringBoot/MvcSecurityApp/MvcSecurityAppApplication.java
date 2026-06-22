1942f70bc77375cba795937dc43607bbb570e7c8
Ojus M S <ojusms@gmail.com>
2026-06-22 16:47:09 -0700

    Initial commit - Spring Boot project from Spring Initializr

---
+++ src/main/java/com/SpringBoot/MvcSecurityApp/MvcSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.MvcSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/MvcSecurityApp/MvcSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.MvcSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
