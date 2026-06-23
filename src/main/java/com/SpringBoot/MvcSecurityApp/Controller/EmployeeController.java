971852787abe9ad7255706050e80edeeb21fe948
Ojus M S <ojusms@gmail.com>
2026-06-22 17:01:39 -0700

    Add Controller package, EmployeeControlle class with a simple mapping for / endpoint, home.html page in the resources folder

---
+++ src/main/java/com/SpringBoot/MvcSecurityApp/Controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.MvcSecurityApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String show() {
+        return "home.html";
+    }
+}
